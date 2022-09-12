bc573c64a911a6ddf23d967ec647d19dd03986de
Yoshimasa Oonishi <y.oonishi.1020@gmail.com>
2022-09-12 20:43:18 +0900

    バグ修正、リファクタリング

--- pages/api/backend/getToken.ts
+++ pages/api/backend/getToken.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { getToken } from 'next-auth/jwt'
 
 
+// バックエンドの変数
 const tokenUrl = 'http://127.0.0.1:8000/token'
 const headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
@@ -11,15 +12,25 @@ const headers = {
 const body = `username=${process.env.BACKEND_USERNAME}&password=${process.env.BACKEND_PASSWORD}`
 
 
+// Next-Auth認証トークンがある場合、バックエンドのアクセストークンを取得する
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse,
 ) {
     const front_token = await getToken({ req })
     if (!front_token) {
-        res.status(401).end()
+        res.status(401).json({"message": "Not signed in"})
+    } else {
+        return backendHandler(req, res)
     }
-    axios.post(tokenUrl, body, {headers: headers}).then((token) => {
-        res.status(200).json(token.data)
-    })
+}
+
+
+// バックエンドのアクセストークンを取得
+async function backendHandler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const backend_token = await axios.post(tokenUrl, body, {headers: headers})
+    res.status(200).json(backend_token.data)
 }
