73e4de70ddff8856dfcc076c89839f3ba91c7765
Yoshimasa Oonishi <y.oonishi.1020@gmail.com>
2022-09-17 17:46:56 +0900

    bug fix / 追加：NextAuthのcallbackにバックエンドのトークン取得

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,13 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import axios from 'axios'
+
+
+// バックエンドの設定
+const tokenUrl = process.env.BACKEND_TOKEN_URL
+const headers = process.env.BACKEND_TOKEN_HEADERS
+const body = `username=${process.env.BACKEND_USERNAME}&password=${process.env.BACKEND_PASSWORD}`
+
 
 export default NextAuth({
     providers: [
@@ -9,4 +17,12 @@ export default NextAuth({
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        // NextAuthのsessionにバックエンドのアクセストークンを追加
+        async session({ session, user, token }) {
+            const backend_token = await axios.post(tokenUrl, body, {headers: headers})
+            session.accessToken = backend_token.data.access_token
+            return session
+        }
+    }
 })
