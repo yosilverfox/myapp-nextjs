b5c79ee9022de3a432f69e5dd450a4b2104afdb6
Yoshimasa Oonishi <y.oonishi.1020@gmail.com>
2022-10-09 18:50:26 +0900

    整理

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useSession, signIn, signOut } from 'next-auth/react'
 import About from '../components/about'
